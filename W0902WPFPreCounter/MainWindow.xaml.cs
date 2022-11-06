f7ff438f26c5e1512ebd089fb66d0729027d3dff
BWOH <bwoh@outlook.kr>
2022-11-07 02:06:17 +0900

    초기 커밋.

---
+++ W0902WPFPreCounter/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace W0902WPFPreCounter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int Count = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAdd(object sender, RoutedEventArgs e)
+        {
+            TextCount.Text = $"{++Count}";
+        }
+
+        private void OnSub(object sender, RoutedEventArgs e)
+        {
+            if (Count > 0)  
+                TextCount.Text = $"{--Count}";
+        }
+    }
+}
